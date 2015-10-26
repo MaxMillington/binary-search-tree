55b1a3be9b7894c3f13671b292a76b000bae8656
MaxMillington <maxmillington89@gmail.com>
2015-10-26 11:03:39 -0600

    removed extra lines

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -15,48 +15,6 @@
 # implementing.
 
 class Node
-  attr_reader :data, :left, :right
-
-  def initialize(data, left = nil, right = nil)
-    @data = data
-    @left = left
-    @right = right
-  end
-
-end
-
-class BinarySearchTree
-
-  attr_reader :root_node
-
-  def initialize(root_node = nil)
-    @root_node = root_node
-  end
-
-  def push(number)
-    if root_node.nil?
-      root_node = Node.new(number)
-    end
-  end
-
-end
-# Node - element of data + connection to
-# other data.
-
-# Nil link means end of list.
-# 1 Dimensional Linked Link [Data|Link] -> [Data|Link]
-
-# Ruby Array is an implementation of a linked list
-
-# Binary Search - For any given node on a binary tree, all
-# elements to the left are less than and all elements to the
-# right are greater than.
-
-# If the data set is already ordered, BST will not be
-# faster than a linked list. So just shuffle before
-# implementing.
-
-class Node
 
   attr_accessor :data, :left, :right
 
